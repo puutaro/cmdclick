2732f0612ab65bae409e0f8641cc1fc9f7926930
kitamura-take <hoperstep@gmail.com>
2022-11-06 09:45:09 +0900

    delete nouse lung setting

--- win/srcs/cmdclick_dir/lib/common_lib/input_gui_lib/launcher_cmd_index_by_lxterminal.sh
+++ win/srcs/cmdclick_dir/lib/common_lib/input_gui_lib/launcher_cmd_index_by_lxterminal.sh
@@ -43,6 +43,5 @@ launcher_cmd_index_by_lxterminal(){
 	done 	
 	powershell.exe -c "add-type -assembly microsoft.visualbasic; [microsoft.visualbasic.interaction]::AppActivate('${WINDOW_TITLE}')" &
 	wait "${lx_terminal_pid}"
-	LANG=C
 }
 
