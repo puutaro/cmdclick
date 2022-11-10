7a9520fc5a7643c5f48fbb71687317c18df9c712
putaro <hoperstep@gmail.com>
2022-11-10 10:43:35 +0900

    bug fix follow that when cmcclick variable value include equal add hyphen in suffix

--- linux/srcs/cmdclick_dir/lib/edit_lib/edit_ini_file_lib/make_ini_contents_lib/echo_by_replace_blank_with_hyphen_and_equal_with_tab.sh
+++ linux/srcs/cmdclick_dir/lib/edit_lib/edit_ini_file_lib/make_ini_contents_lib/echo_by_replace_blank_with_hyphen_and_equal_with_tab.sh
@@ -5,6 +5,6 @@ echo_by_replace_blank_with_hyphen_and_equal_with_tab(){
   local source_con="${1}"
   echo "${source_con}" \
     | sed -e '/^$/d' \
-        -e 's/\=$/=-/' \
+        -re 's/^([^=]*)\=$/\1=-/' \
     | sed -e 's/\=/\t/'
 }
