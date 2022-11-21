3cca46892c9152c542049059c222125c17e00ede
putaro <xbabu@cbabu>
2022-11-21 10:43:49 +0900

    display path hieralky limit to 4

--- linux/srcs/cmdclick_dir/lib/common_lib/echo_longpath_by_summraizing.sh
+++ linux/srcs/cmdclick_dir/lib/common_lib/echo_longpath_by_summraizing.sh
@@ -3,9 +3,11 @@
 
 echo_longpath_by_summraizing(){
 	local path="${1}"
+	local display_path_hierarchy_limit_num=4
 	echo "${path}" \
 		| awk \
 		-v HOME="${HOME}" \
+		-v display_path_hierarchy_limit_num=${display_path_hierarchy_limit_num} \
 		'{
 			target_path = $0
 			gsub(HOME, "~", target_path)
@@ -14,10 +16,10 @@ echo_longpath_by_summraizing(){
 				target_path_list, \
 				"/" \
 			)
-			if(target_path_list_length <= 5){
+			if(target_path_list_length <= display_path_hierarchy_limit_num){
 				print target_path
 				exit
 			}
-			print target_path_list[1]"/"target_path_list[2]"/../"target_path_list[target_path_list_length-1]"/"target_path_list[target_path_list_length]
+			print target_path_list[1]"/../"target_path_list[target_path_list_length-1]"/"target_path_list[target_path_list_length]
 		}'
 }
