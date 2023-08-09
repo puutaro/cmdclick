a264bae4047c7caf06f1d77ed72aada4161fcbda
putaro <hoperstep@gmail.com>
2023-08-09 23:53:37 +0900

    modify fzf installer

--- win/install/after_reboot/ubuntu_setting_lib/install_requirement_package_for_cmdclick.sh
+++ win/install/after_reboot/ubuntu_setting_lib/install_requirement_package_for_cmdclick.sh
@@ -5,7 +5,5 @@ install_requirement_package_for_cmdclick(){
 	sudo apt update -y \
 	&& sudo apt upgrade -y && \
 	sudo apt install -y lxterminal yad xdotool xclip
-	git clone https://github.com/junegunn/fzf.git "$HOME/.fzf" \
-	&& yes \
-	| "$HOME/.fzf/install"
+	sudo apt install -y fzf
 }
